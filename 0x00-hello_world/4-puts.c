5eb121dfe0124089cad4aceeb4b616618bd752df
Kanee Samuel <kaneesamuel79@gmail.com>
2022-09-08 07:59:35 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * By using the function called main - Prints out strings
+ */
+int main(void)
+{
+	/*Inputing the first line of strings*/
+	input ("\"Programming is like building a multilingual puzzle");
+	/*Return 0 if success.*/
+	return (0);
+}
