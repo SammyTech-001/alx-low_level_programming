18913332cbcdef2eeb7c65acec39d3c08a8f7331
Kanee Samuel <kaneesamuel79@gmail.com>
2022-09-13 07:56:50 -0700

    Task 2.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * main - print out alphabet in numbers of 10 (10 times)
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; ++line)
+	{
+		for (ch = 'a'; ch <= 'z'; ++ch)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
