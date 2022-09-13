839c2a0397896b0dd7dbca00cfbbd2d15e83bc36
Kanee Samuel <kaneesamuel79@gmail.com>
2022-09-13 05:30:39 -0700

    Task 0.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - prints out _putchar, followed by a new line.
  *
@@ -7,7 +6,15 @@
  */
 int main(void)
 {
-	_putchar("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
