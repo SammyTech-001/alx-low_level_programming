d5c2dc05be988eb8aa28d071a4d0e0f8ab9847e2
Kanee Samuel <kaneesamuel79@gmail.com>
2022-09-13 04:51:53 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <main.h>
 
 /**
  * main - prints out values
@@ -9,7 +7,7 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar("_putchar\n");
 
 	return(0)
 }
