22e6241e5adb590edc9a9822a96a4af98157e806
Kanee Samuel <kaneesamuel79@gmail.com>
2022-09-13 07:44:55 -0700

    Task 1.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include"main.h"
 
 /**
  * The main - it's an entry point
  *
  * Return: always returns 0
  */
-int 
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		_putchar(ch);
+	_putchar('\n');
+}
