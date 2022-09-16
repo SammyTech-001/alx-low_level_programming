ea80eac21a4a62e2065aaa7819aff716113884e1
Kanee Samuel <kaneesamuel79@gmail.com>
2022-09-16 04:38:36 -0700

    alx low level programming

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#inclued "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: The length of the line
+ */
+
+void print_diagonal(int n)
+{
+	int column, space, i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (colume = 0; column < n; column++)
+		{
+			i = column - i;
+
+			for (space = 0; space <= i; space++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
