06880c3ee7bdaf8a8a314d163101687e2845068c
Kanee Samuel <kaneesamuel79@gmail.com>
2022-09-22 03:16:50 -0700

    alx low level programming

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,18 +10,11 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int index = 0, dest_len = 0;
 
-	i = j = 0;
-	while (*(dest + i))
-		i++;
-	while (j < n && *(scr + j))
-	{
-		*(dest + i) = *(src +j);
-		i++;
-		j++;
-	}
-	if (j < n)
-		*(dest + i) = *(src + j);
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }
