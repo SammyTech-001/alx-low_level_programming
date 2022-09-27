6dc6afd37966e72df924a5b0ef8bf7a75d3c7235
Kanee Samuel <kaneesamuel79@gmail.com>
2022-09-27 02:41:38 -0700

    More Functions Nested Loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code.
  *
  * Return: return 1 for uppercase and 0 for any other
  */
-
 int _isupper(int c)
 {
-	int c = 'A'-'Z'
-
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 	{
-		_putchar("A" c++)
-
 		return (1);
 	}
 	else
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-int -putchar(char c);
+int _putchar(char);
 int _isupper(int c );
 int _isdigit(int c );
 int mul(int a, int b);
